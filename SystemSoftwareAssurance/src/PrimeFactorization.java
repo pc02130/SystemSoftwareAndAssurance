a69f8e0989109b3ecfb82614254dd29e185ffcdc
Perry Campbell <51770549+pc02130@users.noreply.github.com>
2021-03-22 18:51:38 -0400

    Deleted unused methods, cleaned up code

--- SystemSoftwareAssurance/src/PrimeFactorization.java
+++ SystemSoftwareAssurance/src/PrimeFactorization.java
@@ -2,17 +2,15 @@ import java.util.ArrayList;
 
 public class PrimeFactorization {
 	
-	
+/* 
+	Takes an integer as command line argument and prints the prime 
+	factorization for the given integer
+*/
 	public static void main(String[] args) {
-		
 		if (args.length > 0) {		
 			try {
 				int x = Integer.parseInt(args[0]);
 				ArrayList<Integer> facts = factorization(x);
-				if (product(facts) != x) {
-					System.out.println("Something went wrong.");
-					System.exit(0);
-				}
 				System.out.print(x + "=");
 				for(int i = 0; i < facts.size(); i++) {
 					if (i + 1 == facts.size()) {
@@ -25,33 +23,13 @@ public class PrimeFactorization {
 			}
 			catch(Exception e) {
 				System.out.println("Input " + args[0] + " is not a valid decimal integer");
+				System.exit(0);
 			}
 		}
 		else {
 			System.out.println("Enter an integer.");
 			System.exit(0);
-		}
-		
-	}
-	public static boolean isPrime(int num) {
-		if (num > 2 && num % 2 == 0) {
-			return false;
-		}
-		for (int i = 3; i < Math.ceil(num/2); i+=2) {
-			if(num % i == 0) {
-				return false;
-			}
-		}
-		return true;
-		
-	}
-	public static int product(ArrayList<Integer> facts) {
-		int prod = 1;
-		for(int i : facts) {
-			prod *= i;
-			
-		}
-		return prod;
+		}	
 	}
 	public static ArrayList<Integer> factorization(int num) {
 		ArrayList<Integer> facts = new ArrayList<Integer>();
@@ -63,6 +41,5 @@ public class PrimeFactorization {
 		}
 		return facts;
 	}
-
 
 }
